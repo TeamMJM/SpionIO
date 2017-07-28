92a1bbfc49f41edaaa9d64b74bd275ba08bf2537
Jerry Jong <jerryjong01@gmail.com>
2017-07-28 15:04:44 -0700

    refactored scroll data to vanilla javascript

--- server.js
+++ server.js
@@ -9,11 +9,19 @@ let mongoURI = 'mongodb://jerryjong:codesmith123@ds127173.mlab.com:27173/private
 
 mongoose.connect(mongoURI);
 
-app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-  next();
-});
+// app.use(function(req, res, next) {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+//   next();
+// });
+
+app.get('/', (req, res) => {
+    res.sendfile('index.html')
+})
+
+ app.get('/script.js',(req,res,next) =>{
+      res.sendfile('./script.js');
+  })
 
 app.use(bodyParser.json());
 
