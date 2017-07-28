762c4a186e7d745c240589f06d254d86423d0344
Mona Lisa <mustafakhansmt@gmail.com>
2017-07-27 18:26:50 -0700

    Added document readytState

--- script.js
+++ script.js
@@ -1,22 +1,24 @@
-document.onload = (() => {
-    window.addEventListener("click", (e) => {
-        const click = JSON.stringify({
-            clickX: e.clientX,
-            clickY: e.clientY,
-            width: document.width,
-            height: document.height
-        });
-        console.log(click);
-        let request = new XMLHttpRequest();
-        request.open('POST',"http://localhost:3000/storeClick",true);
-        request.setRequestHeader("Content-type","application/json");
-        request.send(click);
-    },false)
+window.onload = (() => {
+    if (document.readyState === 'complete') {
+        window.addEventListener("click", (e) => {
+            const click = JSON.stringify({
+                clickX: e.clientX,
+                clickY: e.clientY,
+                width: document.width,
+                height: document.height
+            });
+            console.log(click);
+            let request = new XMLHttpRequest();
+            request.open('POST', "http://localhost:3000/storeClick", true);
+            request.setRequestHeader("Content-type", "application/json");
+            request.send(click);
+        }, false)
 
-    document.addEventListener((e) => {
-        let sc = document.scrollTop;
-        let bot = window.height + sc;
-        let pageScroll = Math.floor((bot / document.height) * 100);
-        console.log("You've scrolled " + pageScroll + "% of the page");
-    });
+        document.addEventListener((e) => {
+            let sc = document.scrollTop;
+            let bot = window.height + sc;
+            let pageScroll = Math.floor((bot / document.height) * 100);
+            console.log("You've scrolled " + pageScroll + "% of the page");
+        });
+    }
 })
