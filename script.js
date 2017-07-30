72ef80d2aa8c61f94f6741979d49ec25313845ec
Mona Lisa <mustafakhansmt@gmail.com>
2017-07-30 15:28:45 -0700

    Scroll removed

--- script.js
+++ script.js
@@ -47,42 +47,42 @@ window.onload = (() => {
         }, false);
 
 
-        //////////////////// scroll data retrieval ////////////////////
-        let body = document.body,
-            html = document.documentElement;
+        // //////////////////// scroll data retrieval ////////////////////
+        // let body = document.body,
+        //     html = document.documentElement;
 
-        let height = Math.max(body.scrollHeight, body.offsetHeight,
-            html.clientHeight, html.scrollHeight, html.offsetHeight);
-        let pageScroll = 0;
-        document.addEventListener("scroll", (e) => {
-            let sc = window.pageYOffset;
-            let pageScroll = Math.floor((bot / document.documentElement.clientHeight) * 100);
-            console.log("You've scrolled " + pageScroll + "% of the page");
-            socket.emit('storeScroll', pageScroll);
+        // let height = Math.max(body.scrollHeight, body.offsetHeight,
+        //     html.clientHeight, html.scrollHeight, html.offsetHeight);
+        // let pageScroll = 0;
+        // document.addEventListener("scroll", (e) => {
+        //     let sc = window.pageYOffset;
+        //     let pageScroll = Math.floor((bot / document.documentElement.clientHeight) * 100);
+        //     console.log("You've scrolled " + pageScroll + "% of the page");
+        //     socket.emit('storeScroll', pageScroll);
 
-            let bot = document.documentElement.clientHeight + sc;
-            if (pageScroll < Math.floor((bot / height) * 100)) {
-                pageScroll = Math.floor((bot / height) * 100);
-                console.log("You've scrolled " + pageScroll + "% of the page");
-            }
-        }, false);
+        //     let bot = document.documentElement.clientHeight + sc;
+        //     if (pageScroll < Math.floor((bot / height) * 100)) {
+        //         pageScroll = Math.floor((bot / height) * 100);
+        //         console.log("You've scrolled " + pageScroll + "% of the page");
+        //     }
+        // }, false);
 
-        //////////////////// scroll data saving ////////////////////
-        window.addEventListener("beforeunload", (e) => {
-            const scroll = JSON.stringify({
-                scrollPercent: pageScroll
-            });
-            console.log(scroll);
-            let request = new XMLHttpRequest();
-            request.open('POST', "http://localhost:3000/storeScroll", true);
-            request.setRequestHeader("Content-type", "application/json");
-            request.onreadystatechange = function () {
-                if (request.readyState > 3 && request.status == 200) {
-                    console.log(request.responseText);
-                }
-            };
-            request.send(scroll);
-        }, false);
+        // //////////////////// scroll data saving ////////////////////
+        // window.addEventListener("beforeunload", (e) => {
+        //     const scroll = JSON.stringify({
+        //         scrollPercent: pageScroll
+        //     });
+        //     console.log(scroll);
+        //     let request = new XMLHttpRequest();
+        //     request.open('POST', "http://localhost:3000/storeScroll", true);
+        //     request.setRequestHeader("Content-type", "application/json");
+        //     request.onreadystatechange = function () {
+        //         if (request.readyState > 3 && request.status == 200) {
+        //             console.log(request.responseText);
+        //         }
+        //     };
+        //     request.send(scroll);
+        // }, false);
     }
 })
 
