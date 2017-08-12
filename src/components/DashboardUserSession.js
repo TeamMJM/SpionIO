a379bfb4084d6c4074ed028a1c64da82e1da9d3a
Mona Lisa <mustafakhansmt@gmail.com>
2017-08-12 13:10:05 -0700

    Fully Functional Play/Pause

--- src/components/DashboardUserSession.js
+++ src/components/DashboardUserSession.js
@@ -1,8 +1,14 @@
-import React, { Component } from 'react';
-import { Card } from 'material-ui/Card';
+import React, {
+  Component
+} from 'react';
+import {
+  Card
+} from 'material-ui/Card';
 import Playback from './Playback';
 import Storyboard from './Storyboard';
-import { fromJS } from "immutable";
+import {
+  fromJS
+} from "immutable";
 import './../styles/Home.css';
 import axios from 'axios';
 import $ from 'jquery';
@@ -12,120 +18,32 @@ const style = {
   flexFlow: 'row nowrap',
   flex: '1'
 }
+
+let i = 0;
+const REPLAY_SCALE = 0.631;
+const SPEED = 1;
+
+
 class DashboardUserSession extends Component {
 
   constructor(props) {
     super(props);
     this.state = {
-      targetList :[],
-      flag:true,
-      response:null,
-      startPlay:null,
-      i:0
+      targetList: [],
+      flag: true,
+      startPlay: null,
+      $iframeDoc: null,
+      $fakeCursor: null
     }
     this.addtoList = this.addtoList.bind(this);
     this.frameScript = this.frameScript.bind(this);
     this.updateFlag = this.updateFlag.bind(this);
-    this.newDraw = this.newDraw.bind(this);
+    this.getRecordingData = this.getRecordingData.bind(this);
   }
-
-
-
-
-newDraw(){
-  console.log("HELLO");
-  console.log(this.state)
-  response = this.state.response;
-  const REPLAY_SCALE = 0.631;
-  const SPEED = 1;
-  const $iframe = $('.react-iframe');
-
-    $iframe[0].contentDocument.documentElement.innerHTML = response.htmlCopy;
-    const $iframeDoc = $($iframe[0].contentDocument.documentElement);
-
-    const $fakeCursor = $('<div class="cursor"></div>')
-    $iframeDoc.find('body').append($fakeCursor);
-
-    const startPlay = this.state.startPlay;
-    let i = this.state.i;
-  
+  drawAnimate($iframeDoc,$fakeCursor,startPlay,context) {
+    let $iframe = $('.react-iframe');
+    let response = this.state.response;
     (function draw() {
-      let event = response.Frame[this.state.i];
-      if (!event) {
-        return;
-      }
-      let offsetRecording = event.time - response.startTime;
-
-      let offsetPlay = (Date.now() - startPlay) * SPEED;
-      if (offsetPlay >= offsetRecording) {
-        drawEvent(event, $fakeCursor, $iframeDoc);
-        i++;
-      }
-      if (i < response.Frame.length && this.state.flag) {
-        requestAnimationFrame(draw);
-      }
-    })();
-
-    function drawEvent(event, $fakeCursor, $iframeDoc) {
-      if (event.event === 'click' || event.event === 'scroll' || event.event === 'mousemove') {
-        if(event.target){
-          context.addtoList(event.target)
-        }
-
-        if(event.event === "scroll"){
-          $fakeCursor.css({
-            top: event.ClickY,
-            left: event.ClickX
-          })
-          $iframe.contents().scrollTop(event.scrollTop)
-          $iframe.contents().scrollLeft(event.scrollLeft)
-        }
-        else{
-          $fakeCursor.animate({
-            top: event.ClickY,
-            left: event.ClickX
-          },{duration:20,easing:"linear"})
-        }
-      }
-    }  
-}
-
-
-
-
-
-
-
-
-
-
-
-async frameScript(id,context){
-  let response = await axios.get('/recordings/'+id)
-  response = response.data;
-  const REPLAY_SCALE = 0.631;
-  const SPEED = 1;
-  const $iframe = $('.react-iframe');
-
-  $iframe.height(response.height * REPLAY_SCALE);
-  $iframe.width(response.width * REPLAY_SCALE);
-  $iframe.css({
-    '-ms-zoom': `${REPLAY_SCALE}`,
-    '-moz-transform': `scale(${REPLAY_SCALE})`,
-    '-moz-transform-origin': `0 0`,
-    '-o-transform': `scale(${REPLAY_SCALE})`,
-    '-o-transform-origin': `0 0`,
-    '-webkit-transform': `scale(${REPLAY_SCALE})`,
-    '-webkit-transform-origin': `0 0`
-  });    
-  $iframe[0].contentDocument.documentElement.innerHTML = response.htmlCopy;
-    const $iframeDoc = $($iframe[0].contentDocument.documentElement);
-
-    const $fakeCursor = $('<div class="cursor"></div>')
-    $iframeDoc.find('body').append($fakeCursor);
-    let i = 0;
-    const startPlay = Date.now();
-    (async function draw() {
       let event = response.Frame[i];
       if (!event) {
         return;
@@ -140,43 +58,67 @@ async frameScript(id,context){
 
       if (i < response.Frame.length && context.state.flag) {
         requestAnimationFrame(draw);
-      }
-      else if(!this.state.flag){
-        console.log("STATE");
-       await context.setState({
-          response:response,
+      }else if(!context.state.flag){
+        console.log("Coming in");
+        context.setState({
           startPlay:startPlay,
-          i:i
+          $fakeCursor:$fakeCursor,
+          $iframeDoc:$iframeDoc
         })
       }
     })();
 
     function drawEvent(event, $fakeCursor, $iframeDoc) {
       if (event.event === 'click' || event.event === 'scroll' || event.event === 'mousemove') {
-        if(event.target){
+        if (event.target) {
           context.addtoList(event.target)
         }
 
-        if(event.event === "scroll"){
+        if (event.event === "scroll") {
           $fakeCursor.css({
             top: event.ClickY,
             left: event.ClickX
           })
           $iframe.contents().scrollTop(event.scrollTop)
           $iframe.contents().scrollLeft(event.scrollLeft)
-        }
-        else{
+        } else {
           $fakeCursor.animate({
             top: event.ClickY,
             left: event.ClickX
-          },{duration:10,easing:"linear"})
+          }, {
+            duration: 10,
+            easing: "linear"
+          })
         }
       }
     }
+  }
+
+  async frameScript(context) {
+    let response = context.state.response;
+    let $iframe = $('.react-iframe');
+    $iframe.height(response.height * REPLAY_SCALE);
+    $iframe.width(response.width * REPLAY_SCALE);
+    $iframe.css({
+      '-ms-zoom': `${REPLAY_SCALE}`,
+      '-moz-transform': `scale(${REPLAY_SCALE})`,
+      '-moz-transform-origin': `0 0`,
+      '-o-transform': `scale(${REPLAY_SCALE})`,
+      '-o-transform-origin': `0 0`,
+      '-webkit-transform': `scale(${REPLAY_SCALE})`,
+      '-webkit-transform-origin': `0 0`
+    })
+    $iframe[0].contentDocument.documentElement.innerHTML = response.htmlCopy;
+    const $iframeDoc = $($iframe[0].contentDocument.documentElement);
+    let $fakeCursor = $('<div class="cursor"></div>')
     
-}
- 
-  addtoList(element){
+    $iframeDoc.find('body').append($fakeCursor);
+    let i = 0;
+    const startPlay = Date.now();
+    context.drawAnimate($iframeDoc,$fakeCursor,startPlay,context)
+  }
+
+  addtoList(element) {
     let list = this.state.targetList;
     list.push(element)
     this.setState({
@@ -184,30 +126,36 @@ async frameScript(id,context){
     })
   }
 
-  async updateFlag(){
-
-    console.log("UPDATE FLAG",this.state.flag);
+  async updateFlag() {
+    console.log("UPDATE")
     await this.setState({
-        flag: !this.state.flag
+      flag: !this.state.flag
     })
-    if(this.state.flag){
-        this.newDraw()
+    if (this.state.flag) {
+      this.drawAnimate(this.state.$iframeDoc,this.state.$fakeCursor,this.state.startPlay,this)
+    } else {
+      console.log("False")
     }
 
   }
 
+  async getRecordingData() {
+    let response = await axios.get('/recordings/' + this.props.match.params.recordingID);
+    this.setState({
+      response: response.data
+    });
+    this.frameScript(this);
+  }
 
   componentDidMount() {
-
-    console.log('These are the params', this.props.match.params.recordingID)
+    this.getRecordingData();
   }
 
   render() {
-    return(
+    return (
       <div style={style}>
-          <Playback frameScript={this.frameScript} context={this} flag={this.updateFlag} id={this.props.match.params.recordingID}  />
+          <Playback  flag={this.updateFlag} />
           <Storyboard  list={this.state.targetList} />
-          
       </div>
     );
   }
