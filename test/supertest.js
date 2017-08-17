d6d1d8678820e2e1811e8a4dd66999ec62875dad
mm0nr0e <monroe.mirand@gmail.com>
2017-08-17 15:58:59 -0700

    more styles

--- test/supertest.js
+++ test/supertest.js
@@ -17,14 +17,26 @@ describe('/', () => {
           if (err) throw err;
         });
     });
-    it('delivers the html file', () => {
-      request(HOST)
-        .get('/')
-        .expect('Content-Type', /text\/html/)
-        .expect(200)
-        .end(function(err, res) {
-          if (err) throw err;
-        });
-    });
   })
 })
+
+// app.get('/dashboard', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html'));
+// })
+
+// app.get('/dashboard/:recordingID', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html'));
+// })
+
+// app.get('/dashboard/:recordingID/feedback', (req, res) => {
+//     res.sendFile(path.join(__dirname, '/index.html'))
+// })
+
+// app.get('*/build/bundle.js', (req, res, next) => {
+//     res.sendfile('./build/bundle.js');
+// })
+
+// app.get('/recordings', recordingController.findAll)
+// app.get('/recordings/:recordingID', recordingController.findRecording)
+
+// app.get('/frames/:recordingID',frameController.findFrame)
