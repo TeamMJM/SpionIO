7a713440a031c00e1088e4a35847d333b913ef6b
mm0nr0e <monroe.mirand@gmail.com>
2017-08-17 15:16:05 -0700

    more style fixes

--- src/components/DashboardSidebar.js
+++ src/components/DashboardSidebar.js
@@ -5,6 +5,56 @@ import { Link } from 'react-router-dom';
 import Paper from 'material-ui/Paper';
 import { Card } from 'material-ui/Card';
 
+const style = {
+  paper1: {
+    float: 'left', 
+    width: '20%', 
+    height: '750px', 
+    backgroundColor: '#F4F7F5'
+  },
+  paper2: {
+    height: '60px', 
+    margin: '0 auto', 
+    backgroundColor: '#F4F7F5'
+  },
+  p1: {
+    marginTop: '5%', 
+    paddingTop: '5%', 
+    paddingBottom: '5%', 
+    fontSize: '.9em', 
+    width: '90%', 
+    backgroundColor: '#006CAA', 
+    borderRadius: '3px', 
+    color: 'white', 
+    paddingLeft: '4%'
+  }, 
+  p2: {
+    marginTop: '5%', 
+    paddingTop: '5%', 
+    paddingBottom: '5%', 
+    fontSize: '.9em', 
+    color: 'black', 
+    paddingLeft: '7%'
+  },
+  p3: {
+    paddingTop: '5%', 
+    paddingBottom: '5%', 
+    fontSize: '.9em', 
+    width: '90%', 
+    backgroundColor: '#006CAA', 
+    borderRadius: '3px', 
+    color: 'white', 
+    paddingLeft: '4%'
+  },
+  p4: {
+    paddingTop: '5%', 
+    paddingBottom: '5%', 
+    fontSize: '.9em', 
+    color: 'black', 
+    paddingLeft: '7%'
+  }
+}
+
 class DashboardSidebar extends Component {
   constructor(props) {
     super(props);
@@ -19,18 +69,18 @@ class DashboardSidebar extends Component {
 
   styleStorybook() {
     if (this.state.storybook) {
-      return <p style={{marginTop: '5%', paddingTop: '5%', paddingBottom: '5%', fontSize: '.9em', width: '90%', backgroundColor: '#006CAA', borderRadius: '3px', color: 'white', paddingLeft: '4%'}}>Storybook</p>
+      return <p style={style.p1}>Storybook</p>
     } else {
       console.log('storybook not toggled')
-      return <p style={{marginTop: '5%', paddingTop: '5%', paddingBottom: '5%', fontSize: '.9em', color: 'black', paddingLeft: '7%'}}>Storybook</p>
+      return <p style={style.p2}>Storybook</p>
     }
   }
 
   styleSettings() {
     if (this.state.settings) {
-      return <p style={{paddingTop: '5%', paddingBottom: '5%', fontSize: '.9em', width: '90%', backgroundColor: '#006CAA', borderRadius: '3px', color: 'white', paddingLeft: '4%'}}>Settings</p>      
+      return <p style={style.p3}>Settings</p>      
     } else {
-      return <p style={{paddingTop: '5%', paddingBottom: '5%', fontSize: '.9em', color: 'black', paddingLeft: '7%'}}>Settings</p>
+      return <p style={style.p4}>Settings</p>
     }
   }
   
@@ -48,11 +98,11 @@ class DashboardSidebar extends Component {
     <Paper 
       id='customFade2s' 
       className='animated fadeIn' 
-      style={{float: 'left', width: '20%', height: '750px', backgroundColor: '#F4F7F5'}}
+      style={style.paper1}
       >
       <Paper 
         zDepth={0} 
-        style={{height: '60px', margin: '0 auto', backgroundColor: '#F4F7F5'}}
+        style={style.paper2}
         >
         <Link 
           style={{textDecoration: 'none', letterSpacing: '1px'}} 
@@ -64,7 +114,7 @@ class DashboardSidebar extends Component {
       </Paper>
       <Paper 
         zDepth={0} 
-        style={{height: '60px', margin: '0 auto', backgroundColor: '#F4F7F5'}}
+        style={style.paper2}
         >
         <Link 
           style={{textDecoration: 'none', letterSpacing: '1px'}} 
