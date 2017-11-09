b0a4e085ca2e4b9e40a529123ae16f616231c32c
j3rryj <jerryjong01@gmail.com>
2017-11-09 15:47:16 -0800

    fixed Links to point to correct route

--- src/components/DashboardSettings.js
+++ src/components/DashboardSettings.js
@@ -3,7 +3,6 @@ import React, { Component } from 'react';
 // import material-ui components
 import Paper from 'material-ui/Paper'
 
-import './../styles/settings.css';
 
 const style = {
   paper: {
@@ -26,24 +25,22 @@ const style = {
   }
 }
 
-class DashboardSettings extends Component {
-  render() {
-    return(
-      <div className='recording-latest-updates'>
-        <Paper style={style.paper}>
-          <p style={style.pHead}>Your Recording Snippet</p>
-          <hr width='92%' style={{borderColor: 'lightgray', color: 'lightgray', marginTop: '2%', marginBottom: '3%'}}/>
-          <p className='settings-beginning' style={style.pSub}>Copy and paste the script below into the</p>
-          <p className='settings-code' style={style.pSub}>{'<head>'}</p>
-          <p className='settings-end' style={style.pSub}>of every page you wish to start tracking.</p>
+  const DashboardSettings = () => {
+  return(
+    <div className='recording-latest-updates'>
+      <Paper style={style.paper}>
+        <p style={style.pHead}>Your Recording Snippet</p>
+        <hr width='92%' style={{borderColor: 'lightgray', color: 'lightgray', marginTop: '2%', marginBottom: '3%'}}/>
+        <p className='settings-beginning' style={style.pSub}>Copy and paste the script below into the</p>
+        <p className='settings-code' style={style.pSub}>{'<head>'}</p>
+        <p className='settings-end' style={style.pSub}>of every page you wish to start tracking.</p>
 
-          <div style={{marginLeft: '3%', marginTop: '3%', backgroundColor: '#F4F7F5', height: '200px', width: '94%', border: '1px solid #006CAA'}}>
+        <div style={{marginLeft: '3%', marginTop: '3%', backgroundColor: '#F4F7F5', height: '200px', width: '94%', border: '1px solid #006CAA'}}>
 
-          </div>
-        </Paper>
-      </div>
-    )
-  }
-}
+        </div>
+      </Paper>
+    </div>
+  );
+};
 
 export default DashboardSettings;
